05b702d171d879589124711a167ad3992f849577
hjamttes <92005067+hjamttes@users.noreply.github.com>
2024-06-27 12:53:20 -0700

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -5,11 +5,14 @@ const proxyContainer = document.getElementById('proxy-container');
 goButton.addEventListener('click', () => {
   const url = urlInput.value.trim();
   if (url) {
-    fetch(`https://allorigins.win/raw?url=${encodeURIComponent(url)}`)
-     .then(response => response.text())
-     .then(html => {
+    fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`)
+     .then(response => {
+        if (response.ok) return response.json()
+        throw new Error('Network response was not ok.')
+      })
+     .then(data => {
         const parser = new DOMParser();
-        const doc = parser.parseFromString(html, 'text/html');
+        const doc = parser.parseFromString(data.contents, 'text/html');
         const body = doc.body;
         proxyContainer.innerHTML = '';
         proxyContainer.appendChild(body);
